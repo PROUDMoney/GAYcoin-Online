a9e24bcaffb10e24018b69f17eca2491fcde795f
PROUD Money <37021331+PROUDMoney@users.noreply.github.com>
2018-03-05 01:38:31 +0100

    Update rpcmining.cpp

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -117,10 +117,10 @@ Value getworkex(const Array& params, bool fHelp)
         );
 
     if (vNodes.empty())
-        throw JSONRPCError(-9, "GAY Coin is not connected!");
+        throw JSONRPCError(-9, "PROUD Money is not connected!");
 
     if (IsInitialBlockDownload())
-        throw JSONRPCError(-10, "GAY Coin is downloading blocks...");
+        throw JSONRPCError(-10, "PROUD Money is downloading blocks...");
 
     if (pindexBest->nHeight >= LAST_POW_BLOCK)
         throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
@@ -251,10 +251,10 @@ Value getwork(const Array& params, bool fHelp)
             "If [data] is specified, tries to solve the block and returns true if it was successful.");
 
     if (vNodes.empty())
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "GAY Coin is not connected!");
+        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "PROUD Money is not connected!");
 
     if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "GAY Coin is downloading blocks...");
+        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "PROUD Money is downloading blocks...");
 
     if (pindexBest->nHeight >= LAST_POW_BLOCK)
         throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
@@ -395,10 +395,10 @@ Value getblocktemplate(const Array& params, bool fHelp)
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
 
     if (vNodes.empty())
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "GAY Coin is not connected!");
+        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "PROUD Money is not connected!");
 
     if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "GAY Coin is downloading blocks...");
+        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "PROUD Money is downloading blocks...");
 
     if (pindexBest->nHeight >= LAST_POW_BLOCK)
         throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
